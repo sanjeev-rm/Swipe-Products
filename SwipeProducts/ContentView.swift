c46cd3ff173da23a9388e1f21e11df1ed961cfbd
sanjeev-rm <sanjeevraghu2050@gmail.com>
2024-04-15 23:57:09 +0530

    Initial Commit

---
+++ SwipeProducts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwipeProducts
+//
+//  Created by Sanjeev RM on 15/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwipeProducts/SwipeProductsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeProductsApp.swift
+//  SwipeProducts
+//
+//  Created by Sanjeev RM on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
